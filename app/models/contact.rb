a7dffed489423cc6dc22db0138fb4fa1100df05a
Josemar Davi Luedke <josemarluedke@gmail.com>
2011-11-24 12:54:00 -0200

    Created the filters

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,8 +4,19 @@ class Contact < ActiveRecord::Base
 	validates_presence_of :name, :phone, :group_id, :user_id
 
 
-	def self.my_contacts(current_user)
-		where("user_id = #{current_user.id}").all :order => "name"
+	def self.my_contacts(current_user, options = {})
+		where = nil
+		if not options[:favorite].blank?
+			where = "and favorite = true"
+		elsif not options[:letter].blank?
+			where = "and name ILIKE '#{options[:letter]}%'"
+		elsif not options[:search].blank?
+			where = "and name ILIKE '%#{options[:search]}%'"
+		elsif not options[:number].blank?
+			where = "and name ~ '^([0-9]{1,10})'"
+		end
+
+		where("user_id = #{current_user.id} #{where}").all :order => "name"
 	end
 
 	def self.before_save
