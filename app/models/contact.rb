372a64901ad422af2c91dd9ca942cea9d3a7946b
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-01-05 18:01:01 -0200

    Fixed: before_save in Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,33 +1,32 @@
 class Contact < ActiveRecord::Base
-	belongs_to :group
-	belongs_to :user
-	validates_presence_of :name, :phone, :group_id, :user_id
+    belongs_to :group
+    belongs_to :user
+    validates_presence_of :name, :phone, :group_id, :user_id
 
+    def self.my_contacts(current_user, options = {})
+        where = nil
+        if not options[:favorite].blank?
+            where = "and favorite = true"
+        elsif not options[:letter].blank?
+            where = "and name ILIKE '#{options[:letter]}%'"
+        elsif not options[:search].blank?
+            where = "and name ILIKE '%#{options[:search]}%'"
+        elsif not options[:number].blank?
+            where = "and name ~ '^([0-9]{1,})'"
+        end
 
-	def self.my_contacts(current_user, options = {})
-		where = nil
-		if not options[:favorite].blank?
-			where = "and favorite = true"
-		elsif not options[:letter].blank?
-			where = "and name ILIKE '#{options[:letter]}%'"
-		elsif not options[:search].blank?
-			where = "and name ILIKE '%#{options[:search]}%'"
-		elsif not options[:number].blank?
-			where = "and name ~ '^([0-9]{1,})'"
-		end
+        where("user_id = #{current_user.id} #{where}").all :order => "name"
+    end
 
-		where("user_id = #{current_user.id} #{where}").all :order => "name"
-	end
+    before_save do
+        if self.twitter != "" && self.twitter[0...1] != "@"
+            self.twitter = "@#{self.twitter}";
+        end
+    end
 
-	def self.before_save
-		if self.twitter != "" && self.twitter[0...1] != "@"
-			self.twitter = "@#{self.twitter}";
-		end
-	end
-
-	def bookmark_us
-		self.favorite = !self.favorite
-		self.save
-	end
+    def bookmark_us
+        self.favorite = !self.favorite
+        self.save
+    end
 
 end
