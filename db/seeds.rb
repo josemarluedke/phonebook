37becd0be1cf31a75af274c12bfb22b1a67c52c7
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-01-06 00:20:09 -0200

    Created the Seeds for the Groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+groups = ["Work", "Friends", "Family", "Known"]
+4.times do |i|
+    Group.create :name => groups[i-1]
+end
